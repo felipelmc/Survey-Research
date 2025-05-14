33bca3fe6eab2bd0e2f540a4df4fa78635d82959
Felipe <felipe.lamarca@hotmail.com>
2025-05-14 12:03:43 -0300

    Últimas aulas e anotações de Survey -- 14/05/2025

---
+++ listas/lista1/anotacoes.R
@@ -0,0 +1,10 @@
+# era importante fazer algum exercicio de diminuir o numero de estratos
+# pelo menos comparando o tamanho dos estratos, agrupando diferentes estratos
+# no limite se os design effects não é muito diferente então nossas escolhas
+# não foram muito boas
+
+# poderiamos ter comparado o mapa, e aí faria mais sentido juntar coisas proximas
+
+# fora isso, olhar a media de alfabetização é meio que o cheatcode, porque na
+# pratica voce nao teria isso, mas não é errado. Bastaria olhar o numero de 
+# pessoas nos subdistritos e reagrupar
